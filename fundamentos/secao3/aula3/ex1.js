0902f8c2c925a943230fa3a6ff9e13352505868a
Felipe Vergara <felipe.vergara.chico@gmail.com>
2023-03-30 22:37:07 -0300

    aula3

---
+++ fundamentos/secao3/aula3/ex1.js
@@ -0,0 +1,31 @@
+let saldo = 500;
+
+
+function adicioneUmValor (valor) {
+  return valor + saldo;
+   
+};
+
+function subtraiaUmValor (valor) {
+    return saldo - valor;
+};
+
+function dividaUmValor (valor) {
+    return saldo / valor;
+};
+
+function multipliqueUmValor (valor) {
+    return valor * saldo;
+};
+
+
+
+
+
+console.log(adicioneUmValor(300));
+console.log(subtraiaUmValor(200));
+console.log(dividaUmValor(2));
+console.log(multipliqueUmValor(5));
+
+
+
