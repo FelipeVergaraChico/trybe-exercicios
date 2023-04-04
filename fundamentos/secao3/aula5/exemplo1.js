60636923a62e6634eb846aae9d381e314b59366b
Felipe Vergara <felipe.vergara.chico@gmail.com>
2023-04-04 19:03:25 -0300

    exercicios feitos

---
+++ fundamentos/secao3/aula5/exemplo1.js
@@ -0,0 +1,12 @@
+// Com o template literals
+console.log(`Primeira linha;
+Segunda linha;
+Terceira linha;`
+);
+
+// Sem o template literals:
+console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n');
+
+console.log(``)
+
+const contaPalavras = frase => frase.split(' ').length;
